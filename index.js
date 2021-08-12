da61eac770899146872f99b6141c1103ebf9fac7
Lucas DiFalco <luke.difalco52@gmail.com>
2021-08-12 00:05:17 -0400

    started functions for inquirer

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-//First initial commit line
+//create variables with require keyword for prompts through inquirer
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const Manager = require("./lib/Manager");
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+const engineerArray = [];
+const internArray = [];
+const ManagerArray = [];
+
+inquirer
+    .prompt([
+        {
+            type: 'list',
+            name: 'newTeammate',
+            message: 'Select a new teammate'
+            choices: [
+                'Engineer',
+                'Intern',
+                'Team Manager',
+                'No more new employees'
+
+            ]
+        }])
+        .then((choice) => {
+            
+        }
+
+        )
+
+
+
+
+
+
+    ])
+
+
